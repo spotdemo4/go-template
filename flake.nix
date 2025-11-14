84aefacbc474ae38533e55ae08b0e422eda39299
trev <me@trev.xyz>
2025-11-13 23:05:47 -0500

    style: use new nixfmt

--- flake.nix
+++ flake.nix
@@ -27,140 +27,148 @@
     };
   };
 
-  outputs = {
-    nixpkgs,
-    utils,
-    trev,
-    semgrep-rules,
-    ...
-  }:
-    utils.lib.eachDefaultSystem (system: let
-      pkgs = import nixpkgs {
-        inherit system;
-        overlays = [
-          trev.overlays.packages
-          trev.overlays.libs
-        ];
-      };
-    in rec {
-      devShells = {
-        default = pkgs.mkShell {
-          packages = with pkgs; [
-            # go
-            go
-            gotools
-            gopls
-
-            # lint
-            golangci-lint
-            alejandra
-            prettier
-
-            # util
-            air
-            bumper
-          ];
-          shellHook = pkgs.shellhook.ref;
-        };
-
-        update = pkgs.mkShell {
-          packages = with pkgs; [
-            renovate
-
-            # go mod vendor
-            go
+  outputs =
+    {
+      nixpkgs,
+      utils,
+      trev,
+      semgrep-rules,
+      ...
+    }:
+    utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [
+            trev.overlays.packages
+            trev.overlays.libs
           ];
         };
+      in
+      rec {
+        devShells = {
+          default = pkgs.mkShell {
+            packages = with pkgs; [
+              # go
+              go
+              gotools
+              gopls
+              golangci-lint
 
-        vulnerable = pkgs.mkShell {
-          packages = with pkgs; [
-            # go
-            go
-            govulncheck
+              # util
+              air
+              bumper
 
-            # nix
-            flake-checker
+              # nix
+              nixfmt
 
-            # actions
-            octoscan
-          ];
+              # actions
+              prettier
+            ];
+            shellHook = pkgs.shellhook.ref;
+          };
+
+          update = pkgs.mkShell {
+            packages = with pkgs; [
+              renovate
+
+              # go mod vendor
+              go
+            ];
+          };
+
+          vulnerable = pkgs.mkShell {
+            packages = with pkgs; [
+              # go
+              go
+              govulncheck
+
+              # nix
+              flake-checker
+
+              # actions
+              octoscan
+            ];
+          };
         };
-      };
 
-      checks = pkgs.lib.mkChecks {
-        go = {
-          src = packages.default;
-          deps = with pkgs; [
-            golangci-lint
-            opengrep
-          ];
-          script = ''
-            go test ./...
-            golangci-lint run ./...
-            opengrep scan \
-              --quiet \
-              --error \
-              --use-git-ignore \
-              --exclude="/vendor/" \
-              --config="${semgrep-rules}/go"
-          '';
+        checks = pkgs.lib.mkChecks {
+          go = {
+            src = packages.default;
+            deps = with pkgs; [
+              golangci-lint
+              opengrep
+            ];
+            script = ''
+              go test ./...
+              golangci-lint run ./...
+              opengrep scan \
+                --quiet \
+                --error \
+                --use-git-ignore \
+                --exclude="/vendor/" \
+                --config="${semgrep-rules}/go"
+            '';
+          };
+
+          nix = {
+            src = ./.;
+            deps = with pkgs; [
+              nixfmt
+            ];
+            script = ''
+              nixfmt -c flake.nix
+            '';
+          };
+
+          actions = {
+            src = ./.;
+            deps = with pkgs; [
+              prettier
+              action-validator
+              octoscan
+              renovate
+            ];
+            script = ''
+              prettier --check .
+
+              # github
+              action-validator .github/**/*.yaml
+              octoscan scan .github
+              renovate-config-validator .github/renovate.json
+
+              # gitea
+              action-validator .gitea/**/*.yaml
+              octoscan scan .gitea
+              renovate-config-validator .gitea/renovate.json
+            '';
+          };
         };
 
-        nix = {
-          src = ./.;
-          deps = with pkgs; [
-            alejandra
-          ];
-          script = ''
-            alejandra -q -c flake.nix
-          '';
-        };
+        packages.default = pkgs.buildGoModule (finalAttrs: {
+          pname = "go-template";
+          version = "0.1.6";
 
-        actions = {
-          src = ./.;
-          deps = with pkgs; [
-            prettier
-            action-validator
-            octoscan
-            renovate
-          ];
-          script = ''
-            prettier --check .
+          src = builtins.path {
+            name = "root";
+            path = ./.;
+          };
+          goSum = finalAttrs.src + "go.sum";
+          vendorHash = null;
+          env.CGO_ENABLED = 0;
 
-            # github
-            action-validator .github/**/*.yaml
-            octoscan scan .github
-            renovate-config-validator .github/renovate.json
+          meta = {
+            description = "go template";
+            mainProgram = "go-template";
+            homepage = "https://github.com/spotdemo4/go-template";
+            changelog = "https://github.com/spotdemo4/go-template/releases/tag/v${finalAttrs.version}";
+            license = pkgs.lib.licenses.mit;
+            platforms = pkgs.lib.platforms.all;
+          };
+        });
 
-            # gitea
-            action-validator .gitea/**/*.yaml
-            octoscan scan .gitea
-            renovate-config-validator .gitea/renovate.json
-          '';
-        };
-      };
-
-      packages.default = pkgs.buildGoModule (finalAttrs: {
-        pname = "go-template";
-        version = "0.1.6";
-        src = builtins.path {
-          name = "root";
-          path = ./.;
-        };
-        goSum = finalAttrs.src + "go.sum";
-        vendorHash = null;
-        env.CGO_ENABLED = 0;
-
-        meta = {
-          description = "go template";
-          mainProgram = "go-template";
-          homepage = "https://github.com/spotdemo4/go-template";
-          changelog = "https://github.com/spotdemo4/go-template/releases/tag/v${finalAttrs.version}";
-          license = pkgs.lib.licenses.mit;
-          platforms = pkgs.lib.platforms.all;
-        };
-      });
-
-      formatter = pkgs.alejandra;
-    });
+        formatter = pkgs.nixfmt;
+      }
+    );
 }
