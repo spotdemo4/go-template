b69c0ec7e663d7e3823bca219af626401b49f319
trev <me@trev.xyz>
2026-05-11 07:09:03 -0400

    feat: zed, oxc

--- flake.nix
+++ flake.nix
@@ -13,8 +13,8 @@
   inputs = {
     systems.url = "github:spotdemo4/systems";
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
-    trev = {
-      url = "github:spotdemo4/nur";
+    trevpkgs = {
+      url = "github:spotdemo4/trevpkgs";
       inputs.systems.follows = "systems";
       inputs.nixpkgs.follows = "nixpkgs";
     };
@@ -23,10 +23,10 @@
   outputs =
     {
       self,
-      trev,
+      trevpkgs,
       ...
     }:
-    trev.libs.mkFlake (
+    trevpkgs.libs.mkFlake (
       system: pkgs: {
 
         # nix develop [#...]
@@ -36,22 +36,23 @@
             packages = with pkgs; [
               # go
               go
-              gotools
               gopls
+              gotools
 
               # lint
               go-tools
+              oxlint
               nixd
+              nil
 
               # format
-              treefmt
-              prettier
+              oxfmt
               nixfmt
-              tombi
+              treefmt
 
               # util
-              air
               bumper
+              fix-hash
             ];
           };
 
@@ -71,23 +72,22 @@
             packages = with pkgs; [
               renovate
               go # go mod vendor
+              fix-hash # vendorHash
             ];
           };
 
           vulnerable = pkgs.mkShell {
             packages = with pkgs; [
+              govulncheck # go
               flake-checker # nix
               zizmor # actions
-              govulncheck # go
             ];
           };
         };
 
         # nix run [#...]
         apps = pkgs.mkApps {
-          default = "go run .";
-          dev = "air";
-          vendor = "go mod tidy && go mod vendor";
+          dev = "go run .";
         };
 
         # nix build [#...]
@@ -103,11 +103,21 @@
                   ./go.mod
                   ./go.sum
                   (fileset.fileFilter (file: file.hasExt "go") ./.)
-                  (fileset.maybeMissing ./vendor)
                 ];
               };
               goSum = ./go.sum;
+              proxyVendor = true;
               vendorHash = null;
+
+              nativeCheckInputs = with pkgs; [
+                go-tools
+              ];
+              checkPhase = ''
+                export HOME=$(mktemp -d)
+                go test ./...
+                go vet ./...
+                staticcheck ./...
+              '';
 
               meta = {
                 mainProgram = "go-template";
@@ -133,24 +143,18 @@
         formatter = pkgs.treefmt.withConfig {
           configFile = ./treefmt.toml;
           runtimeInputs = with pkgs; [
-            prettier
-            nixfmt
             go
-            tombi
+            oxfmt
+            nixfmt
           ];
         };
 
         # nix flake check
         checks = pkgs.mkChecks {
-          prettier = {
-            root = ./.;
-            filter = file: file.hasExt "yaml" || file.hasExt "json" || file.hasExt "md";
-            ignore = pkgs.lib.fileset.maybeMissing ./vendor;
-            packages = with pkgs; [
-              prettier
-            ];
-            forEach = ''
-              prettier --check "$file"
+          go = self.packages.${system}.default.overrideAttrs {
+            dontBuild = true;
+            installPhase = ''
+              touch $out
             '';
           };
 
@@ -161,7 +165,7 @@
             packages = with pkgs; [
               nixfmt
             ];
-            forEach = ''
+            script = ''
               nixfmt --check "$file"
             '';
           };
@@ -173,7 +177,7 @@
               action-validator
               zizmor
             ];
-            forEach = ''
+            script = ''
               action-validator "$file"
               zizmor --offline "$file"
             '';
@@ -190,28 +194,14 @@
             '';
           };
 
-          go = {
-            src = self.packages.${system}.default;
+          config = {
+            root = ./.;
+            filter = file: file.hasExt "json" || file.hasExt "yaml" || file.hasExt "toml" || file.hasExt "md";
             packages = with pkgs; [
-              go-tools
+              oxfmt
             ];
             script = ''
-              go test ./...
-              go vet ./...
-              staticcheck ./...
-            '';
-          };
-
-          tombi = {
-            root = ./.;
-            filter = file: file.hasExt "toml";
-            ignore = pkgs.lib.fileset.maybeMissing ./vendor;
-            packages = with pkgs; [
-              tombi
-            ];
-            forEach = ''
-              tombi format --offline --check "$file"
-              tombi lint --offline --error-on-warnings "$file"
+              oxfmt --check
             '';
           };
         };
